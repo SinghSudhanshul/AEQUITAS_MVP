6718f3910722072e5a49e287ef38f562474138d9
BOSS <pearson.spector.litt0@gmail.com>
2026-02-04 12:51:45 +0530

    Frontend

---
+++ frontend/src/components/wings/donnas-desk/index.ts
@@ -0,0 +1,6 @@
+export * from './DonnaConversationPanel';
+export * from './SmartTaskQueue';
+export * from './CalendarIntegration';
+export * from './NotificationCenter';
+export * from './DelegationManager';
+export * from './ProactiveInsightsPanel';
---
+++ frontend/src/components/wings/harveys-office/index.ts
@@ -0,0 +1,6 @@
+export * from './HarveyConsultation';
+export * from './ExecutiveStrategyBoard';
+export * from './TeamPerformanceOverview';
+export * from './DecisionQueue';
+export * from './ExecutiveSummaryCharts';
+export * from './MemoArchive';
